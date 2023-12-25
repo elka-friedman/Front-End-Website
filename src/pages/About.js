8b391fb84d4f58b5546900e9a7d3332f36c150ea
Elka Friedman <elkafriedman1@gmail.com>
2023-12-25 13:03:03 -0500

    lorem ipsum

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
         <div className="about">
             <div
                 className="aboutTop"
-            style={{ backgroundImage: `url(${MultiplePizzas})` }}
+                style={{ backgroundImage: `url(${MultiplePizzas})` }}
             ></div>
             <div className="aboutBottom">
                 <h1> ABOUT US</h1>
@@ -18,7 +18,7 @@ function About() {
                     fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
                     accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
                     molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-                    officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+                    officia aut! Impedit sit sunt quaerat, Lorem Ipsum odit, tenetur error, harum
                     nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
                     error repudiandae fuga? Ipsa laudantium molestias eos sapiente
                     officiis modi at sunt excepturi expedita sint? Sed quibusdam
